8ed386fbc181e36e297277f3a6369f8c434b677c
urain39 <urain39@qq.com>
2023-05-14 03:52:43 +0800

    update charging range (55~75)

--- service.sh
+++ service.sh
@@ -13,7 +13,7 @@ CHARGING_ENABLED="/sys/class/power_supply/battery/charging_enabled"
         echo "0" > "$CHARGING_ENABLED"
       fi
     elif [ "$status" = "Discharging" ]; then
-      if [ "$capacity" -le "35" ]; then
+      if [ "$capacity" -le "55" ]; then
         echo "1" > "$CHARGING_ENABLED"
       fi
     fi
